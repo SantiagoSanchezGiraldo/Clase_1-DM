c825e6ffc875c6681066d60a2b0f5b4900809242
SantiagoSanchezGiraldo <santiagosanchezgiraldo@pascualbravo.edu.co>
2024-02-06 09:15:19 -0500

    Variables
    Introduciendo variables en Java

--- app/src/main/java/com/app_1santisg/MainActivity.java
+++ app/src/main/java/com/app_1santisg/MainActivity.java
@@ -6,6 +6,16 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean result=true;
+    String name = "Santiago Sánchez Giraldo";
+    int num = 17;
+    float num7 = (float)0.4;
+    char grade = 'A';
+    byte b = 20;
+    long num1 = 1234567;
+    short no = 10;
+    double pi = 3.14;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
